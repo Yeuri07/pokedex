b386e5104565c57ea5ad1955073b98c5b35fb536
Yeuri07 <Trabajo_utesad@hotmail.com>
2023-08-23 14:39:55 -0400

    pokemon view change

--- index.js
+++ index.js
@@ -1,58 +1,44 @@
 
 function fetchPokemon(id){
 
-    const URL_POKEAPI = `https://pokeapi.co/api/v2/pokemon/${id}`;
-
-    return URL_POKEAPI;
-}
-
-function fetchPokemon(id) {
-    
-}
-
-fetch(URL_POKEAPI)
+fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
 .then(res => res.json())
 .then(response =>{
     console.log(response)
    
 const contenedor = document.querySelector(".flex-container");
 
-function crearPokemon(nombre,clase,habilidad){
+function crearPokemon(nombre,id,habilidad){
  
-    img = `<img class='pokemon' src='${response.sprites.back_default}'>`;
-    nombre = `<h2 class='color-text'>${nombre}</h2>`;
-    clase = `<h3><b>${clase}</b></h3>`;
+    id = `<div class='color-text id-pokemon'>
+    <span>ID</span>
+    <h3 ><b>#${response.id.toString().padStart(3,0)}</b></h3>
+    
+    </div>`;
+    img = `<img class='pokemon' src='${response.sprites.other.dream_world.front_default}'>`;
+    nombre = `<h2 class='color-text'>${response.name}</h2>`;
     habilidad = `<p>habilidad: <b>$${habilidad}</b></p>`;
 
-    return [img,nombre,clase,habilidad]
+    return [id,img,nombre,habilidad]
 }
 
 let documentFragment = document.createDocumentFragment();
 
-for(var i =1; i<=10; i++){
-    let claseRandom = Math.round(Math.random()*10000);
-    let habilidadRandom = Math.round(Math.random()*10+50);
-    let pokemon = crearPokemon(`pokemon ${i}`,`clase: ${claseRandom}`,habilidadRandom)
+    let pokemon = crearPokemon(`pokemon`)
     let div = document.createElement("DIV");
-    div.addEventListener("click",()=>{
-        document.querySelector(".key-data").value = claseRandom
-    })
-    div.tabIndex = i;
-    div.classList.add(`item-${i}`,'flex-item');
+    div.classList.add("container-pokemon");
     div.innerHTML = pokemon.join(" ")
     documentFragment.appendChild(div)
     
-}
-
 contenedor.appendChild(documentFragment)
 
 }).catch(e => console.error(new Error(e)));
 
-
-function fetchPokemons(number){
-    for(let i = 1; i< number; i++){
+}
+function fetchPokemons(){
+    for(let i = 1; i<= 16; i++){
         fetchPokemon(i)
     }
 }
 
-fetchPokemons(9)
+fetchPokemons()
