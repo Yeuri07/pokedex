08cb4ffdb8e05f9214b8617d252991e26f3c0154
Yeuri07 <Trabajo_utesad@hotmail.com>
2023-08-24 23:04:40 -0400

    adding pokemon finder

--- index.js
+++ index.js
@@ -1,38 +1,71 @@
+const input = document.getElementById('input__pokemon')
+
 const cargarPokemon = async () =>{
+ 
+    const valorDeInput = input.value;
+  
     try{
-    const responde = await fetch('https://pokeapi.co/api/v2/pokemon/')
+    const responde = await fetch(`https://pokeapi.co/api/v2/pokemon/${valorDeInput}`);
 
-    if(responde.status === 200){
-        const date = await responde.json();
-        console.log(date)
-        let pokemons = '';
+    
+    const partes = responde.url.split('/');
 
-        date.results.forEach((pokemon,index) => {
+    const ultimoFramento = partes[partes.length - 1];
+
+    if(ultimoFramento === ''){
+
+        if(responde.status === 200){
+            const date = await responde.json();
+        
+            let pokemons = '';
+    
+            date.results.forEach((pokemon,index) => {
+               
+                pokemons += `
+                <div class="container-pokemon">
+                    <div class="id-pokemon"><span>ID</span><span>#${(index+1).toString().padStart(3,0)}</span></div>
+                    <img class='pokemon' src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${index+1}.svg">
+                    <h3 class="color-text">${pokemon.name}</h3>
+                </div>`
+               
+            });
            
-            pokemons += `
-            <div class="container-pokemon">
-                <div class="id-pokemon"><span>ID</span><span>#${(index+1).toString().padStart(3,0)}</span></div>
-                <img class='pokemon' src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${index+1}.svg">
-                <h3 class="color-text">${pokemon.name}</h3>
-            </div>`
-            console.log((index+1).toString().padStart(3,0))
-        });
+            document.getElementById('container').innerHTML = pokemons;
+    
+        }else if(responde.status === 401){
+            console.log("Error no existe.")
+        }else if(responde.status === 404){
+            console.log("Pokemon no Existe!}.")
+        }else{
+            console.error("Error Unplanned !! ")
+        }
+    
+    } else {
+
+
+        let pokemons = '';
        
-        document.getElementById('container').innerHTML = pokemons;
+        fetch(responde.url)
+        .then((responde) => responde.json())
+        .then((date) =>{
+            pokemons += `<div class="container-pokemon solo">
+            <div class="id-pokemon"><span>ID</span><span>#${(date.id).toString().padStart(3,0)}</span></div>
+            <img class='pokemon' src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${date.id}.svg">
+            <h3 class="color-text">${date.name}</h3>
+        </div>`
 
-    }else if(responde.status === 401){
-        console.log("Error no existe.")
-    }else if(responde.status === 404){
-        console.log("Pokemon no Existe!}.")
-    }else{
-        console.error("Error Unplanned !! ")
-    }
-
+        console.log(date)
+            document.getElementById('container').innerHTML = pokemons;
+        })
+}
+    
+    
 
     }catch(error){
         console.error(error)
     } 
 }
 
+const boton = document.getElementById('miBoton');
 
-cargarPokemon();
+boton.addEventListener('click', cargarPokemon);
