aecc404e4be0d4f9ba5df8d0aff8e7502ee06ee3
Yeuri07 <Trabajo_utesad@hotmail.com>
2023-09-06 20:22:27 -0400

    agregando logica de los tipos de elemento

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
-const input = document.getElementById('input__pokemon')
-
+ 
+const input = document.getElementById('input__pokemon');
 const cargarPokemon = async () =>{
  
     const valorDeInput = input.value;
   
     try{
     const responde = await fetch(`https://pokeapi.co/api/v2/pokemon/${valorDeInput}`);
+    console.log(responde)
+   
 
-    
+
     const partes = responde.url.split('/');
 
     const ultimoFramento = partes[partes.length - 1];
@@ -16,16 +18,20 @@ const cargarPokemon = async () =>{
 
         if(responde.status === 200){
             const date = await responde.json();
-        
+   
+
             let pokemons = '';
-    
+           
             date.results.forEach((pokemon,index) => {
-               
+                
+              
                 pokemons += `
                 <div class="container-pokemon">
                     <div class="id-pokemon"><span>ID</span><span>#${(index+1).toString().padStart(3,0)}</span></div>
                     <img class='pokemon' src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${index+1}.svg">
+                    <div class="pokemon-tipos"></div>
                     <h3 class="color-text">${pokemon.name}</h3>
+                    
                 </div>`
                
             });
@@ -44,17 +50,18 @@ const cargarPokemon = async () =>{
 
 
         let pokemons = '';
-       
+      
         fetch(responde.url)
         .then((responde) => responde.json())
         .then((date) =>{
+            
             pokemons += `<div class="container-pokemon solo">
             <div class="id-pokemon"><span>ID</span><span>#${(date.id).toString().padStart(3,0)}</span></div>
             <img class='pokemon' src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${date.id}.svg">
             <h3 class="color-text">${date.name}</h3>
+          
         </div>`
 
-        console.log(date)
             document.getElementById('container').innerHTML = pokemons;
         })
 }
